99244afbdeee8cec361ee84c9e9b3334da1a837b
marvelm57 <vivalamarvel@gmail.com>
2024-04-28 00:17:36 +0700

    Improve login and register page

--- pengguna/views.py
+++ pengguna/views.py
@@ -1,34 +1,14 @@
 from django.shortcuts import render, redirect
 
 from django.contrib import messages 
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth import authenticate, login, logout
+# from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
-    form = UserCreationForm()
-
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Your account has been successfully created!')
-            return redirect('main:login')
-    context = {'form':form}
-    return render(request, 'register.html', context)
+    return render(request, 'register.html')
 
 def login_user(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('main:show_main')
-        else:
-            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
-    context = {}
-    return render(request, 'login.html', context)
+    return render(request, 'login.html')
 
 def logout_user(request):
-    logout(request)
     return redirect('pengguna:login')
