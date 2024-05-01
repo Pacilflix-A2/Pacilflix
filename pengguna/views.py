1c67310ed2abac97a104e2344784f62e08204a56
Hafizmuh18 <hafizmuh18@gmail.com>
2024-05-01 23:40:48 +0700

    Change Font

--- pengguna/views.py
+++ pengguna/views.py
@@ -1,47 +1,41 @@
 import datetime
 from django.shortcuts import render, redirect
 
-from django.contrib import messages 
-from django.contrib.auth import authenticate, login, logout
-from pengguna.forms import UserRegisterForm
-from django.http import HttpResponseRedirect
-from django.urls import reverse
 
 def register(request):
-    if request.user.is_authenticated:
-        return HttpResponseRedirect(reverse("main:show_main")) # Ubah ke halaman daftar tayangan
+    # if request.user.is_authenticated:
+    #     return HttpResponseRedirect(reverse("main:show_main")) # Ubah ke halaman daftar tayangan
 
-    form = UserRegisterForm()
+    # form = UserRegisterForm()
 
-    if request.method == "POST":
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Your account has been successfully created!')
-            return redirect('pengguna:login')
-    context = {'form':form}
-    return render(request, 'register.html', context)
+    # if request.method == "POST":
+    #     form = UserRegisterForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         messages.success(request, 'Your account has been successfully created!')
+    #         return redirect('pengguna:login')
+    # context = {'form':form}
+    return render(request, 'register.html')
 
 def login_user(request):
-    if request.user.is_authenticated:
-        return HttpResponseRedirect(reverse("main:show_main"))  # Ubah ke halaman daftar tayangan
+    # if request.user.is_authenticated:
+    #     return HttpResponseRedirect(reverse("main:show_main"))  # Ubah ke halaman daftar tayangan
         
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    # if request.method == 'POST':
+    #     username = request.POST.get('username')
+    #     password = request.POST.get('password')
 
-        user = authenticate(request, username=username, password=password)
+    #     user = authenticate(request, username=username, password=password)
 
-        if user is not None:
-            login(request, user)
-            response = HttpResponseRedirect(reverse("main:show_main")) 
-            response.set_cookie('last_login', str(datetime.datetime.now()))
-            return response
-        else:
-            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
-    context = {}
-    return render(request, 'login.html', context)
+    #     if user is not None:
+    #         login(request, user)
+    #         response = HttpResponseRedirect(reverse("main:show_main")) 
+    #         response.set_cookie('last_login', str(datetime.datetime.now()))
+    #         return response
+    #     else:
+    #         messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    # context = {}
+    return render(request, 'login.html')
 
 def logout_user(request):
-    logout(request)
     return redirect('main:show_main')
